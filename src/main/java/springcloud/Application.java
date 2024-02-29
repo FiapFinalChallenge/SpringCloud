4c25cf4ee964060db7969118a5a26b90e610f92d
Marcos653 <marcos.costas@xbrain.com.br>
2024-02-29 18:46:00 -0300

    feat: implement apis routes

--- src/main/java/springcloud/Application.java
+++ src/main/java/springcloud/Application.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class SpringCloudApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
